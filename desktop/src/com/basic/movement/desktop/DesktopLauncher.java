65433cc5ae3e40a50c922304eb7048df4b1a397b
alesaurio <aleperaltabazas@est.frba.utn.edu.ar>
2019-01-10 15:43:36 -0300

    project structure and basic assets

---
+++ desktop/src/com/basic/movement/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.basic.movement.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.basic.movement.BasicMovementGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BasicMovementGame(), config);
+	}
+}
