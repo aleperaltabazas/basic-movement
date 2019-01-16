5f07508a32bdcafc9314ab83392c255824b68bc4
alesaurio <aleperaltabazas@est.frba.utn.edu.ar>
2019-01-16 12:54:23 -0300

    fix for map

--- core/assets/maps/emerald_exteriors.tsx
+++ core/assets/maps/emerald_exteriors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="emerald_exteriors" tilewidth="16" tileheight="16" tilecount="6072" columns="88">
- <image source="C:/Users/Usuario/Downloads/61816.png" width="1408" height="1104"/>
+ <image source="tilesets/61816.png" width="1408" height="1104"/>
 </tileset>
