f36a2f3bc3f862205b02e371c4bbb1ebdb945fdd
Alejandro <aleperaltabazas@hotmail.com>
2019-01-20 12:40:43 -0300

    adding input manager

---
+++ core/src/com/freemovement/game/movement/InputManager.java
@@ -0,0 +1,50 @@
+package com.freemovement.game.movement;
+
+import com.badlogic.gdx.InputAdapter;
+
+import static com.freemovement.game.movement.KeyAdapter.*;
+
+public class InputManager extends InputAdapter {
+    private boolean movingWest;
+    private boolean movingEast;
+    private boolean movingNorth;
+    private boolean movingSouth;
+
+    private boolean running;
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case NORTH:
+                return movingNorth = true;
+            case SOUTH:
+                return movingSouth = true;
+            case EAST:
+                return movingEast = true;
+            case WEST:
+                return movingWest = true;
+            case RUNNING:
+                return running = true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case NORTH:
+                return !(movingNorth = false);
+            case SOUTH:
+                return !(movingSouth = false);
+            case EAST:
+                return !(movingEast = false);
+            case WEST:
+                return !(movingWest = false);
+            case RUNNING:
+                return !(running = false);
+            default:
+                return false;
+        }
+    }
+}
---
+++ core/src/com/freemovement/game/movement/KeyAdapter.java
@@ -0,0 +1,11 @@
+package com.freemovement.game.movement;
+
+import com.badlogic.gdx.Input;
+
+public class KeyAdapter {
+    public static final int NORTH = Input.Keys.UP;
+    public static final int SOUTH = Input.Keys.DOWN;
+    public static final int EAST = Input.Keys.RIGHT;
+    public static final int WEST = Input.Keys.LEFT;
+    public static final int RUNNING = Input.Keys.X;
+}
